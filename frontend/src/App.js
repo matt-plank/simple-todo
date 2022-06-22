f839eb394b736ed931119fc6c0506b37f33f3903
Matt Plank <matthew_plank@outlook.com>
2022-06-22 17:20:01 +0100

    Added bootstrap and some app template

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,32 @@
-import { Fragment } from 'react';
+import { Fragment, useState } from 'react';
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      text: 'My simple task',
+    },
+    {
+      text: 'Your simple task',
+    },
+  ]);
+
   return (
     <Fragment>
-      <h1>Hello, World!</h1>
+      <div className="container mt-5">
+        <h1 className="text-white bg-secondary p-3">Simple To-Do App</h1>
+
+        <div className="container mt-5">
+          <h2 className="m-5">Tasks</h2>
+          <div className="form-check">
+            {tasks.map((task) => (
+              <p>
+                <input className="form-check-input" type="checkbox" />
+                {task.text}
+              </p>
+            ))}
+          </div>
+        </div>
+      </div>
     </Fragment>
   );
 }
